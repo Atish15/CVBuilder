a5e20075ee0004535c33c3aa8c1a787240e591fc
aatishgyawali <aatishgyawali10@gmail.com>
2023-04-12 18:06:56 +0100

    Correcting port

--- server.js
+++ server.js
@@ -103,8 +103,11 @@ let moduleDelete=tempArr.forEach(async (dat)=>{
 
 
 //Start the app listening on port 8080
-app.listen(8080);
-console.log("Server listening on port 8080");
+const port= process.env.PORT || 8080;
+app.listen(port,function (){
+    console.log("Server listening on port: "+port);
+});
+
 
 
 //Export server for testing
